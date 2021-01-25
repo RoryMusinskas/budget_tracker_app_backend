0595dfdf5fdd4cea7728b914d0a3cfbf0926e506
RoryMusinskas <devrorym@gmail.com>
2021-01-25 16:22:33 +1100

    Removed a lot of unneeded code in this controller. It was initially getting the auth0 UserManagement  authentication token passed in, but this token was creating the no consent error, unless you added a second consent popup. Instead I have refactored this to just use an email passed in from the login check (I had all this code just to get an email). Now it's a lot cleaner

--- app/controllers/secured_controller.rb
+++ app/controllers/secured_controller.rb
@@ -12,45 +12,17 @@ class SecuredController < ApplicationController
   # Run the authorization process on the request coming in
   def authorize_request
     @decoded_request = AuthorizationService.new(request.headers).authenticate_request!
-    # call the get_user_details function
-    get_user_details
+    set_class_attribute
   rescue JWT::VerificationError, JWT::DecodeError
     render json: { errors: ['Not Authenticated'] }, status: :unauthorized
   end
   
-  # Get the user details from the Auth0 Management endpoint 
-  def get_user_details
-    # Get the sub from the user request
-    user_sub = @decoded_request[0]['sub']
-
-    # Encode the url for better processing
-    encoded_url = URI.encode("https://dev-2fj3w1-h.au.auth0.com/api/v2/users/#{user_sub}")
-    url = URI.parse(encoded_url)
-
-    # Make a new HTTP request to Auth0 user endpoint
-    http = Net::HTTP.new(url.host, url.port)
-    http.use_ssl = true
-    http.verify_mode = OpenSSL::SSL::VERIFY_NONE
-
-    request = Net::HTTP::Get.new(url)
-    request["authorization"] = "Bearer #{params['UserAccessToken']}"
-
-    response = http.request(request)
-    user = JSON.parse(response.body)
-    
-    # Once we have user data, set the Class attributes to pass to children
-    set_class_attribute(user)
-
-  end
-  
   # Once we have user data, set the Class attributes to pass to children
-  def set_class_attribute(user)
+  def set_class_attribute
     # Update these details only when the user endpoint is checked to see if the user exists or not (This is ran every refresh)
     if params[:controller] == 'users'
-      SecuredController.current_user = user['email']
-      SecuredController.current_user_id = User.where(email: SecuredController.current_user).ids[0]
-    else
-      @current_user = SecuredController.current_user
+      SecuredController.current_user = params[:user][:email]
+      SecuredController.current_user_id = User.where(email: params[:user][:email]).ids[0]
     end
   end
 end
