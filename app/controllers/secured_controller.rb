a5916fe728b591066590d74dce5ecac1d87f52fc
RoryMusinskas <devrorym@gmail.com>
2021-01-28 16:17:04 +1100

    Removed all the unneeded code and have set the current user based on the user sub from auth0

--- app/controllers/secured_controller.rb
+++ app/controllers/secured_controller.rb
@@ -12,18 +12,9 @@ class SecuredController < ApplicationController
   # Run the authorization process on the request coming in
   def authorize_request
     @decoded_request = AuthorizationService.new(request.headers).authenticate_request!
-    set_class_attribute
+    @current_user = @decoded_request[0]['sub']
   rescue JWT::VerificationError, JWT::DecodeError
     render json: { errors: ['Not Authenticated'] }, status: :unauthorized
-  end
-  
-  # Once we have user data, set the Class attributes to pass to children
-  def set_class_attribute
-    # Update these details only when the user endpoint is checked to see if the user exists or not (This is ran every refresh)
-    if params[:controller] == 'users' && params[:action] == 'create'
-      SecuredController.current_user = params[:user][:email]
-      SecuredController.current_user_id = User.where(email: params[:user][:email]).ids[0]
-    end
   end
 end
 
