b6744dd855248639648a6ef99e9816f6cab98a33
RoryMusinskas <devrorym@gmail.com>
2021-02-03 19:22:49 +1100

    Removed the star from the origins to see if this fixes the no cors error

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins '*, https://dazzling-liskov-5232da.netlify.app'
+    origins 'https://dazzling-liskov-5232da.netlify.app'
 
     resource '*',
       headers: :any,
